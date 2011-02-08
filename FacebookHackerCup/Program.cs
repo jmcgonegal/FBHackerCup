26776bc7f52a764776d67615c38a18c945c9e5b5
John McGonegal <mcgonegal@gmail.com>
2011-02-08 16:12:47 -0700

    first commit, round 1 passed

---
+++ FacebookHackerCup/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace FacebookHackerCup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int MAX = 2147483647;
+            FileInfo file = new FileInfo("input.txt");
+            int max_sq = (int)Math.Ceiling(Math.Sqrt((double)MAX)); // ~46341
+            int[] array = new int[max_sq+1];
+
+            // do some math ahead of time
+            for (int i = 0; i <= max_sq; i++)
+            {
+                array[i] = i * i;
+            }
+
+            StreamReader fs = new StreamReader( file.OpenRead());
+
+            int count = int.Parse(fs.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(calcDoubleSquare(array, int.Parse(fs.ReadLine())));
+            }
+        }
+        static int calcDoubleSquare(int[] squares, int num)
+        {
+            int count = 0;
+            for (int i = 0; i < squares.Length; i++)
+            {
+                if (squares[i] > num) break;
+                for (int j = i; j < squares.Length; j++)
+                {
+                    if (squares[i] + squares[j] > num) { break; }
+                    else if (squares[i] + squares[j] == num) { count++; break; }
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ StudiousStudent/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace StudiousStudent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo file = new FileInfo("input.txt");
+            StreamReader fs = new StreamReader(file.OpenRead());
+
+            int count = int.Parse(fs.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] values = fs.ReadLine().Split(' ');
+                int index = int.Parse(values[0]);
+                string[] strings = new string[index];
+
+                Array.Copy(values, 1, strings, 0, index);
+                List<string> sorted = new List<string>(strings);
+                sorted.Sort();
+                foreach (string s in sorted)
+                {
+                    Console.Write(s);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
